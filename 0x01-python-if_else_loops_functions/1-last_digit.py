cb1e510fe29ba602ab63f01f57326328794ae011
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-21 07:24:13 -0800

    typo error fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ else:
     aux = number
 print("Last digit of {:d} is {:d}".format(number, aux % 10), end=" ")
 if aux % 10 > 5:
-    print('and is greater than 5')
+    print("and is greater than 5")
 elif aux % 10 == 0:
-    print('and is 0')
+    print("and is 0")
 else:
-    print('and is less than 6 and not 0')
+    print("and is less than 6 and not 0")
