971db8f7b8294d0c810b38ad86da3f8eeaeafab5
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-21 09:09:39 -0800

    aux fixed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,8 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     aux = number % 10
 else:
-    aux = -number % 10
-print("Last digit of",number,"is", aux, end=" ")
+    aux = -number % 10 * -1
+print("Last digit of {:d} is {:d}".format(number, aux), end=" ")
 if aux > 5:
     print("and is greater than 5")
 elif aux < 6 and aux != 0:
