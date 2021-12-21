220526c93940391d04fe8e913608a6caa1ae9964
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-21 08:44:35 -0800

    some conditions changed of place

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 print("Last digit of {:d} is {:d}".format(number, aux % 10), end=" ")
 if aux % 10 > 5:
     print("and is greater than 5")
-elif aux % 10 == 0:
-    print("and is 0")
-else:
+elif aux % 10 < 6 and aux % 10 != 0:
     print("and is less than 6 and not 0")
+else:
+    print("and is zero")
