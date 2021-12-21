9b31e1f419ca6de196fbf5b44fd4a63e6d2838dd
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-21 07:26:15 -0800

    changed the assignament of the aux

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    aux = number * -1
-else:
+if number >= 0:
     aux = number
+else:
+    aux = number * -1
 print("Last digit of {:d} is {:d}".format(number, aux % 10), end=" ")
 if aux % 10 > 5:
     print("and is greater than 5")
