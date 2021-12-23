6d3f26ede573ba0aec9cd804af92de9b39fe6b92
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-23 07:09:17 -0800

    First commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+if len(sys.argv) == 2:
+    print("{:d} argument:\n1: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) != 2:
+    print("{:d} arguments:".format((len(sys.argv) - 1), end=""))
+    for i in range (1, len(sys.argv)):
+        print("{:d} {:s}".format(i, sys.argv[i]))
