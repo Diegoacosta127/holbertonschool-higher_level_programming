c26de0b1b4a81da644798e113b9fa099cf0851e0
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-23 12:41:52 -0800

    6. Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] != '+' and sys.argv[2] != '-'\
+       and sys.argv[2] != '*' and sys.argv[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if sys.argv[2] == '+':
+        print("{} {} {} = {}"
+              .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
+                      int(sys.argv[1]) + int(sys.argv[3])))
+    if sys.argv[2] == '-':
+        print("{} {} {} = {}"
+              .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
+                      int(sys.argv[1]) - int(sys.argv[3])))
+    if sys.argv[2] == '*':
+        print("{} {} {} = {}"
+              .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
+                      int(sys.argv[1]) * int(sys.argv[3])))
+    if sys.argv[2] == '/':
+        print("{} {} {} = {}"
+              .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
+                      int(sys.argv[1]) / int(sys.argv[3])))
