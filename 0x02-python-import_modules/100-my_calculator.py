fcd0e6ed46c669bcde2eba8efec5535e6f1cef29
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-23 12:54:14 -0800

    output error restored and now it uses the imported functions

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 4:
-        print("./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] != '+' and sys.argv[2] != '-'\
        and sys.argv[2] != '*' and sys.argv[2] != '/':
@@ -13,16 +13,16 @@ if __name__ == "__main__":
     if sys.argv[2] == '+':
         print("{} {} {} = {}"
               .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
-                      int(sys.argv[1]) + int(sys.argv[3])))
+                      add(int(sys.argv[1]), int(sys.argv[3]))))
     if sys.argv[2] == '-':
         print("{} {} {} = {}"
               .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
-                      int(sys.argv[1]) - int(sys.argv[3])))
+                      sub(int(sys.argv[1]), int(sys.argv[3]))))
     if sys.argv[2] == '*':
         print("{} {} {} = {}"
               .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
-                      int(sys.argv[1]) * int(sys.argv[3])))
+                      mul(int(sys.argv[1]), int(sys.argv[3]))))
     if sys.argv[2] == '/':
         print("{} {} {} = {}"
               .format(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]),
-                      int(sys.argv[1]) / int(sys.argv[3])))
+                      div(int(sys.argv[1]), int(sys.argv[3]))))
