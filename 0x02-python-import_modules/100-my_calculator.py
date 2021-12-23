0a05fcce027328326749d28d8f1b1c31cd6b58fb
Diegoacosta127 <diegoacosta127@gmail.com>
2021-12-23 12:48:40 -0800

    output fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] != '+' and sys.argv[2] != '-'\
        and sys.argv[2] != '*' and sys.argv[2] != '/':
