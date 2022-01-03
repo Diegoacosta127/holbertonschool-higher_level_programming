8c2b857aebf72cb208a5ef715039e8915dc6129f
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-03 09:19:18 -0800

    logical issues fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            a = 0
-            for a in range(len(row)):
-                if a < len(row) - 1:
-                    print("{:d} ".format(row[a]), end="")
-                else:
-                    print("{:d}".format(row[a]))
+    for row in matrix:
+        for idx in row:
+            print("{:d} ".format(idx), end="")
+            if idx != row[-1]:
+                print(" ", end="")
+        print("")
