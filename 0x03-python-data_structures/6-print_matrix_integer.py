d24538e6c0ba3a6f0117ac0bbb6816d00e1de970
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-03 09:21:40 -0800

    output issue fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for idx in row:
-            print("{:d} ".format(idx), end="")
+            print("{:d}".format(idx), end="")
             if idx != row[-1]:
                 print(" ", end="")
         print("")
