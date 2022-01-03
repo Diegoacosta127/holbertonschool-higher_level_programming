e9fabbe1b352be9fb29ee057f39a149b56affb4d
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-03 08:55:16 -0800

    existence of matrix check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        a = 0
-        for a in range(len(row)):
-            if a < len(row) - 1:
-                print("{:d} ".format(row[a]), end="")
-            else:
-                print("{:d}".format(row[a]))
+    if matrix:
+        for row in matrix:
+            a = 0
+            for a in range(len(row)):
+                if a < len(row) - 1:
+                    print("{:d} ".format(row[a]), end="")
+                else:
+                    print("{:d}".format(row[a]))
