1d5c59844d3009979a06d5df8f7f932c6e523963
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-18 11:32:20 -0800

    documentation added

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""4. Access and update private attribute"""
+
+
 class Square:
+    """Class: Square"""
     def __init__(self, size=0):
+        """constructor method"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,6 +15,7 @@ class Square:
 
     @property
     def size(self):
+        """int: getter"""
         return self.__size
 
     @size.setter
@@ -22,4 +28,5 @@ class Square:
             self.__size = size
 
     def area(self):
+        """Calculates the area of a square"""
         return self.__size * self.__size
