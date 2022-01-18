09f29ff38ffcaec35a2a761f1cd4dfbdcd58fff0
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-18 11:20:46 -0800

    documentation added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""1. Square with size"""
+
+
 class Square:
+    """Class name: Square"""
     def __init__(self, size=0):
+        """Constructor method"""
         self.__size = size
