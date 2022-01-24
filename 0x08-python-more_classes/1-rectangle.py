2f97e1ca71b540ffd6ecc4de10ce96587e94ffeb
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-24 13:59:32 -0800

    output fixes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,27 +6,18 @@ class Rectangle:
     """class doc"""
     def __init__(self, width=0, height=0):
         """init doc"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        elif not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.height = height
-            self.width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
         """width getter doc"""
-        return self.width
+        return self.__width
 
     @property
     def height(self):
         """height getter doc"""
-        return self.height
+        return self.__height
 
     @width.setter
     def width(self, width):
