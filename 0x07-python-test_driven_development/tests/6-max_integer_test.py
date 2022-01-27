b21a955c9d2716807dfb21e4d9092a525c1e829c
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-26 16:46:48 -0800

    border case fixed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,4 +13,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
         self.assertEqual(max_integer([1, 3, 2]), 3)
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([-1]), -1)
