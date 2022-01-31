09d42b62fdb35215949569e5fdfb725894464ae4
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-31 13:58:49 -0800

    output fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """module doc"""
 
+
 def inherits_from(obj, a_class):
     """method doc"""
+    if type(obj) == a_class:
+        return False
     return issubclass(type(obj), a_class)
