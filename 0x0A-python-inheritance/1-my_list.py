18ab1c5437a4fe0461a06bd0ab2ec71ad5b8e38c
Diegoacosta127 <diegoacosta127@gmail.com>
2022-01-31 13:49:38 -0800

    output fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,7 @@ class MyList(list):
     """class doc"""
     def print_sorted(self):
         """print_sorted doc"""
+        for item in self:
+            if type(item) is not int:
+                raise TypeError("must be a list of integers")
         print(sorted(self))
