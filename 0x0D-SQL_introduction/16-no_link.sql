0666bfe170e841d02b68229d0889641fd082ae30
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-08 10:52:06 -0800

    first commit

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- 16. Say my name
+-- A script that lists all records of the table second_table of the database hbtn_0c_0 in a MySQL server
+SELECT score, name FROM second_table WHERE EXISTS(SELECT name) ORDER BY score DESC
