b430bd84c0e2addc336aea273e5996b864ff7343
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-08 08:35:28 -0800

    first commit

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- 8. Count 89
+-- A script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in a MySQL server
+SELECT COUNT( * ) as total_record FROM first_table WHERE id=89
