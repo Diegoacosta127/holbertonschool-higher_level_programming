c6703402d43be5dcc524c4807dae41bd5ef79589
Diegoacosta127 <diegoacosta127@gmail.com>
2022-03-08 14:48:16 -0800

    first commit

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- 17. Go to UTF8
+-- A script that converts hbtn_0c_0 database ti UTF8(utf8mb4, collate utf8mb4_unicode_ci) in a MySQL server
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
