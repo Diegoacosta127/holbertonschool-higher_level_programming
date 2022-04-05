084f499581481f77251076005ec4abcc27b670ad
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-05 08:02:41 -0700

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,12 +6,13 @@ class Rectangle {
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write('X');
       }
-	console.log('');
+      console.log('');
     }
   }
 }
