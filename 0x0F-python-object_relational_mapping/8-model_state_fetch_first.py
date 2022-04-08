8c6d09729f4dea65d01ef34619b412e70d1e0265
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-08 09:12:47 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module doc"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                       argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+if (len(session.query(State).order_by(State.id).all()) < 1):
+    print()
+else:
+    state = session.query(State).order_by(State.id).all()
+    print("{}: {}".format(state[0].id, state[0].name,))
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module doc"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                       argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all():
+    if 'a' in state.name or 'A' in state.name:
+        print("{}: {}".format(state.id, state.name,))
