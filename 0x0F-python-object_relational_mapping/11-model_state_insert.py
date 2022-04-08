1de1004be5aa7af24b013dad2a015a5305662d73
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-08 10:31:32 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module doc"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                       argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+new_state = State()
+new_state.name = 'Louisiana'
+session.add(new_state)
+session.commit()
+for state in session.query(State).order_by(State.id).all():
+    if state.name == new_state.name:
+        print("{}".format(state.id))
