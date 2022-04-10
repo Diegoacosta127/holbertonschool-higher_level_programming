5e2a75850ac4bf8567bf81e03a2f27cd24e693ab
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-09 21:07:22 -0700

    guard added for execution

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,16 +4,18 @@ import MySQLdb
 from sys import argv
 
 
-db = MySQLdb.connect(host='localhost',
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3],
-                     port=3306)
-cur = db.cursor()
-cur.execute("SELECT cities.id, cities.name,\
-            states.name FROM cities\
-            JOIN states ON cities.state_id=states.id ORDER BY cities.id ASC")
-result = cur.fetchall()
-if result:
-    for i in result:
-        print(i)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name,\
+                states.name FROM cities\
+                JOIN states ON cities.state_id=states.id\
+                ORDER BY cities.id ASC")
+    result = cur.fetchall()
+    if result:
+        for i in result:
+            print(i)
