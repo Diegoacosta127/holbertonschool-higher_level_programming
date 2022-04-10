cb656e93fcc59b9c7fc42290e259aebe64643a3b
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-09 22:04:11 -0700

    hope the checker likes it nowgit add 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """module doc"""
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from requests import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
@@ -12,9 +12,10 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    session = Session(engine)
-    state = session.query(State).order_by(State.id).all()
-    if state is None:
-        print("Nothing")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print("{}: {}".format(state[0].id, state[0].name,))
+        print("Nothing")
