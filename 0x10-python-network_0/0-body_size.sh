935592175bdb1985c4e6b685ca8ff9fc527e9ab3
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-22 15:01:01 -0300

    argument fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script is used to calculate the body size of a HTTP request.
-curl -sI 0.0.0.0:5000 | grep "Content-Length" | cut -d " " -f 2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f 2
