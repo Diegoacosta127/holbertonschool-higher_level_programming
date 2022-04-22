37509a040f5c0dc1389be95dc045297b3e2269e0
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-22 22:06:51 +0000

    some branches issue

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-#comment
-curl -sX POST -H 'Content-Type:application/json' -d "@$2" "$1"
-=======
 #Sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -sX POST -H 'Content-Type:application/json' -d "@$2" "$1"
->>>>>>> a7f41bb526445084098addc05c93ce23dc35bb8a
