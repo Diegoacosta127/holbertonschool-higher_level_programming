13fdb881e29a4546a342e747410269d8827d4a69
Diegoacosta127 <diegoacosta127@gmail.com>
2022-04-23 20:29:37 +0000

    comments modified?

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,13 +2,13 @@
 """
 Python script that takes in a letter and sends a POST request to a url with
 the letter as a parameter
-* Letter is sent in the variable q 
-* If no argument is given, sets q=""
-* If the response body is properly JSON formatted and not empty, displays the
+Letter is sent in the variable q 
+If no argument is given, sets q=""
+If the response body is properly JSON formatted and not empty, displays the
 id and name like this: [<id>] <name>
-* Otherwise:
-    Display 'Not a valid JSON' if the JSON is invalid
-    Display 'No result' if the JSON is empty
+Otherwise:
+- Display 'Not a valid JSON' if the JSON is invalid
+- Display 'No result' if the JSON is empty
 """
 import requests
 from sys import argv
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     r = requests.post('http://0.0.0.0:5000/search_user', data)
     try:
         req = r.json()
-        if len(req) > 0:
-            print("[{}] {}".format(req.get('id'), req.get('name')))
-        else:
+        if len(req) == 0:
             print("No result")
+        else:
+            print("[{}] {}".format(req.get('id'), req.get('name')))
     except:
         print("Not a valid JSON")
